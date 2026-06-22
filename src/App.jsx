2e95572168fa9a867977cb389757bc5387bffd34
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-06-22 23:11:31 +1000

    feat(toastify): set up React‑Toastify with base configuration and global styles

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Button from 'react-bootstrap/Button';
+import { ToastContainer,toast } from 'react-toastify';
+
+function App() {
+ 
+
+  return (
+    <><h1>hellow</h1>
+    <Button variant="primary">Primary</Button>
+    <ToastContainer />
+    </>
+  )
+}
+
+export default App
