fa3eb89be731ff12969e58933188f03efb93c2a9
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-08-05 11:14:46 +1000

    feat: update table rendering logic, add balance column, and apply color formatting

--- src/components/layout/TransactionTable.jsx
+++ src/components/layout/TransactionTable.jsx
@@ -1,5 +1,6 @@
 import Table from 'react-bootstrap/Table';
 import { useUser } from '../../context/UserContext';
+import { Button, FormControl } from 'react-bootstrap';
 
 export const TransactionTable = () => {
 
@@ -8,7 +9,13 @@ export const TransactionTable = () => {
     return transaction.type === "income" ? acc + transaction.amount : acc - transaction.amount;
   }, 0);
   return (
-    <Table striped bordered hover>
+    <>
+    <div className='d-flex justify-content-between align-items-center pt-3 mb-3'>
+      <div>{transactions.length} transactions found </div>
+      <div> <FormControl type ="text"/></div>
+      <div><Button className="primary">Add New Transaction</Button></div>
+    </div>
+    <Table striped  hover>
       <thead>
         <tr>
           <th>#</th>
@@ -27,29 +34,27 @@ export const TransactionTable = () => {
               <td>{transaction.title}</td>
               {/* for expense  or income conditions*/}
               {transaction.type === "expense" && ( <>
+              <td className="debit">$${transaction.amount}</td>
               <td></td>
-              <td>$${transaction.amount}</td>
               </>)
               }
               {transaction.type === "income" && ( <>
-              
-              <td>$${transaction.amount}</td>
               <td></td>
+              <td className="credit">$${transaction.amount}</td>
               </>)
               }
-              
-              
             </tr>
           ))
         }
         
-        
         <tr className = "font-weight-bold text-end">
           <td colSpan={3}>Total Balance</td>
-          <td colSpan={3}>${totalBalance.toFixed(2)}</td>
+          <td colSpan={3} className={totalBalance >= 0 ? "text-success" : "text-danger"} > ${totalBalance.toFixed(2)}</td>
         </tr>
       </tbody>
     </Table>
+    </>
+    
   );
 }
 export default TransactionTable;
